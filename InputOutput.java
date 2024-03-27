6a086a718aa5ff102f5828eaf3293aefc12a0229
nizarrizani <xxnizarrizani@gmail.com>
2024-03-28 03:47:19 +0800

    Compile & Run

---
+++ InputOutput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ Public static void main(String [] args){
+ Scanner scanner = new scanner(System.in);
+ String nama;
+ System.out.print("Masukkan nama anda: ");
+ nama = scanner.nextLine();
+ System.out.println("Hai, "+nama);
+ }
+}
